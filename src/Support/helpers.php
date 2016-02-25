6c4a237bce246111261a547b0c582848c54ec201
Carlos Yakimov <logicode.ve@gmail.com>
2016-02-25 17:59:08 -0300

    Added `env` function to read environment variables

--- src/Support/helpers.php
+++ src/Support/helpers.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Support\Str;
+
 if (! function_exists('asset')) {
     /**
      * Generate an asset path for the application theme.
@@ -97,5 +99,40 @@ if (! function_exists('request')) {
             return app('request');
         }
         return app('request')->getParam($key, $default);
+    }
+}
+
+if (! function_exists('env')) {
+    /**
+     * Gets the value of an environment variable. Supports boolean, empty and null.
+     *
+     * @param  string  $key
+     * @param  mixed   $default
+     * @return mixed
+     */
+    function env($key, $default = null)
+    {
+        $value = getenv($key);
+        if ($value === false) {
+            return value($default);
+        }
+        switch (strtolower($value)) {
+            case 'true':
+            case '(true)':
+                return true;
+            case 'false':
+            case '(false)':
+                return false;
+            case 'empty':
+            case '(empty)':
+                return '';
+            case 'null':
+            case '(null)':
+                return;
+        }
+        if (Str::startsWith($value, '"') && Str::endsWith($value, '"')) {
+            return substr($value, 1, -1);
+        }
+        return $value;
     }
 }
